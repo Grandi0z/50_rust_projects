645a9e18fc147635a38ee15220fe5a8c2b5fbd7e
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2024-06-17 19:06:11 +0200

    initial

---
+++ read_csv/src/main.rs
@@ -0,0 +1,18 @@
+use csv;
+use std::error::Error;
+fn main() {
+    if let Err(e) = read_from_file("./file.csv") {
+        eprintln!("Error: {}", e);
+    }
+}
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
