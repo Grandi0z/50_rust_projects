2001d4e8bfec5512d4f0888d53756a03649ae48b
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2024-06-21 22:58:05 +0200

    api call

---
+++ target/debug/api_call.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/target/debug/api_call: /mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/api_call/src/main.rs
---
+++ target/debug/deps/api_call-c0a05a0e6d89cc03.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/target/debug/deps/api_call-c0a05a0e6d89cc03: api_call/src/main.rs
+
+/mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/target/debug/deps/api_call-c0a05a0e6d89cc03.d: api_call/src/main.rs
+
+api_call/src/main.rs:
