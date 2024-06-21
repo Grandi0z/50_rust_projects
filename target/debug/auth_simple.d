c9678d50fd283816d987e0faa76b4c03ea4f5ee1
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2024-06-21 23:31:21 +0200

    auth_simple

---
+++ target/debug/auth_simple.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/target/debug/auth_simple: /mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/auth_simple/src/main.rs
---
+++ target/debug/deps/auth_simple-1cb108ea3366724b.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/target/debug/deps/auth_simple-1cb108ea3366724b: auth_simple/src/main.rs
+
+/mnt/c/Users/Joseph/Documents/CLASS/Rust/exercises/50_project_akhil_sharma/target/debug/deps/auth_simple-1cb108ea3366724b.d: auth_simple/src/main.rs
+
+auth_simple/src/main.rs:
